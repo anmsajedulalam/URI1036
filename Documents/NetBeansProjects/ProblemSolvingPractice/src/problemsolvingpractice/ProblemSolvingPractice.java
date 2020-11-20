d1cb1feb4dfd6fd71ebd98395131503673f1d266
Sajid <sajidcsebu@gmail.com>
2020-11-20 20:29:55 +0600

    hello

---
+++ Documents/NetBeansProjects/ProblemSolvingPractice/src/problemsolvingpractice/ProblemSolvingPractice.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problemsolvingpractice;
+import java.util.*;
+/**
+ *
+ * @author sajid
+ */
+public class ProblemSolvingPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner k=new Scanner(System.in);
+    }
+    
+}
